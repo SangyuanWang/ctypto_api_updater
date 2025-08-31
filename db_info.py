59fb11ed9ab64a14540cf4876931ddfdad82b4a9
smangj <631535207@qq.com>
2025-08-31 15:43:05 +0800

    write_to_db加入自动检查对齐

--- db_info.py
+++ db_info.py
@@ -7,12 +7,22 @@ author: smangj
 mail: 631535207@qq.com
 """
 from sqlalchemy import create_engine
-
+from sqlalchemy import inspect
 
 DB_URL = "postgresql://postgres:Vk3rUTQjcweSkOGO@abel-sit.cwbe6kuqcn4q.us-east-1.rds.amazonaws.com:5432/abel-test"
 
-
 def write_to_db(df, table_name, db_url=DB_URL):
     engine = create_engine(db_url)
-    with engine.connect() as co:
+    # --- 对齐 DataFrame 列到表结构 ---
+    insp = inspect(engine)
+    table_cols = [col['name'] for col in insp.get_columns(table_name)]
+
+    # 补齐缺失列
+    for col in table_cols:
+        if col not in df.columns:
+            df[col] = None
+
+    # 丢掉多余列
+    df = df[table_cols]
+    with engine.begin() as co:
         df.to_sql(table_name, co, if_exists="append", index=False, method='multi')
